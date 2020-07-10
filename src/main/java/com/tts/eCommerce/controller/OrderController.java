b198599742adb32bc76e472e5cb0e3a6ce4df9ee
Anwar Akkari <mohamedanwar.akkari@yale.edu>
2020-07-10 12:16:40 -0400

    Update

--- src/main/java/com/tts/eCommerce/controller/OrderController.java
+++ src/main/java/com/tts/eCommerce/controller/OrderController.java
@@ -1,5 +1,17 @@
 package com.tts.eCommerce.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tts.eCommerce.model.Cart;
+
 public class OrderController {
+	
+	@GetMapping("/checkout")
+	public String checkout (Cart cart, Model model) {
+		Order purchaseOrder = orderService.makePurchaseOrder(cart);
+		model.addAttribute("purchaseOrder", purchaseOrder);
+		return "storefront/invoice;
+	}
 
 }
--- src/main/java/com/tts/eCommerce/service/OrderService.java
+++ src/main/java/com/tts/eCommerce/service/OrderService.java
@@ -3,6 +3,8 @@ package com.tts.eCommerce.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tts.eCommerce.model.Cart;
+import com.tts.eCommerce.model.Order;
 import com.tts.eCommerce.repository.OrderRepository;
 
 @Service
@@ -10,5 +12,24 @@ public class OrderService {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+
+	
+	
+	public Order makePurchaseOrder(Cart cart) {
+		//Order thatObjectThatBecomesAnOrderFromACart.(whateverthefield) = new Order();
+		// code to populate fields to create an order object
+		//return thatObjectThatBecomesAnOrderFromACart;
+		return null;
+	}
+	
+	
+	//method to calculate shipping and tax and calculate a total
+	private Double calcSalesTax(Double subTotal) {
+		//math for tax	
+		Double salesTax = 0.1 * subTotal;
+		return salesTax;
+	}
+
+
 
 }
